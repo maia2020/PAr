082dce4b0de49f32cbd6746f20e470d6c4dafc33
Matheus Bortolin <matheus.bortolin@usp.br>
2023-10-22 17:23:34 +0200

    new file:   Documentos/Gantt.gan new file:   Documentos/Gantt.pdf new file:   Documentos/InscriptionPAR-105.pdf new file:   Documentos/PERTT (1).jpg new file:   Documentos/PERTT.drawio new file:   Tunable.py

---
+++ Tunable.py
@@ -0,0 +1,6 @@
+"""Code basé sur l'article "Tunable bandgaps in a deployable metamaterial" pour reproduire la matrice de transfert"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sy
